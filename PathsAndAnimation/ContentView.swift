1ba33adfc1f4f3317fb8d70dbe508c92fe68032a
SchwiftyUI <schwiftyui@gmail.com>
2019-10-05 17:07:18 -0700

    Initial Commit

---
+++ PathsAndAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PathsAndAnimation
+//
+//  Created by SchwiftyUI on 10/5/19.
+//  Copyright © 2019 SchwiftyUI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
